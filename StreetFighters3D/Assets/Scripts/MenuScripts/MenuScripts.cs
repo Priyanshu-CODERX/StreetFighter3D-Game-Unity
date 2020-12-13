3bbe076dce87981e68b9f88d545da5254f0ff78e
Priyanshu-CODERX <pbo4oh@gmail.com>
2020-12-13 22:34:19 +0530

    Added Game Files

---
+++ StreetFighters3D/Assets/Scripts/MenuScripts/MenuScripts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+    public void instructions(){
+    	SceneManager.LoadScene("instructions");
+    }
+
+    public void loadMainMenu(){
+    	SceneManager.LoadScene("StartMenu");
+    }
+
+    public void loadCredits(){
+    	SceneManager.LoadScene("credits");
+    }
+
+    public void loadGame(){
+    	SceneManager.LoadScene("MainLevel");
+    }
+
+    public void quit(){
+    	Application.Quit();
+    }
+}
---
+++ StreetFighters3D/Assets/Scripts/MenuScripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+	private string sceneLoad = "MainLevel";
+    public void PlayGame() {
+    	SceneManager.LoadScene(sceneLoad);
+    }
+
+    public void QuitGame() {
+    	Debug.Log("QUITTING GAME");
+    	Application.Quit();
+    }
+
+}
